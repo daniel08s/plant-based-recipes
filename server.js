505ca57e1e01618a976856eaff13b94b71c8e223
iskilled <iskilled@gmail.com>
2018-08-21 19:25:02 +0100

    Add Apollo-Express middleware

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: 'variables.env' });
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// Set up GraphQL-Express middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+// create GraphQL schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+// Connects to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err));
+
+// Initializes express server
+const app = express();
+
+// Create GraphiQL application
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Connect Mongo schemas to GraphQL
+app.use('/graphql', graphqlExpress({
+  schema,
+  context: {
+    Recipe,
+    User
+  }
+}));
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
