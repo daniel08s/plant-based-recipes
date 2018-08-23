e6a7e95c55130840a9bf69ed4becc1296269057d
iskilled <iskilled@gmail.com>
2018-08-23 22:36:37 +0100

    Create registration form with state management

---
+++ client/src/components/Auth/Signup.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { username, email, password, passwordConfirmation} = this.state;
+    return (
+      <div className="App">
+        <h2 className="App">Signup</h2>
+        <form className="form">
+          <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={this.handleChange}
+          />
+          <input
+          type="text"
+          name="email"
+          placeholder="Email address"
+          value={email}
+          onChange={this.handleChange}
+          />
+          <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={this.handleChange}
+          />
+          <input
+          type="password"
+          name="passwordConfirmation"
+          placeholder="Confirm password"
+          value={passwordConfirmation}
+          onChange={this.handleChange}
+          />
+          <button type="submit" className="button-primary">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Signup;
