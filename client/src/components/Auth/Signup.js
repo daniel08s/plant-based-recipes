b42b51daeb780da7d47caa84e854b33d21c82b76
iskilled <iskilled@gmail.com>
2018-08-23 23:21:55 +0100

    Add signup mutations in apollo and on the form. Create Error component. Add basic form validation.

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
+
+import { SIGNUP_USER } from '../../queries';
+import Error from '../Error';
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  passwordConfirmation: ""
+};
 
 class Signup extends React.Component {
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    passwordConfirmation: ""
+  state = { ...initialState };
+
+  clearState = () => {
+    this.setState({ ...initialState });
   };
 
   handleChange = event => {
@@ -13,42 +23,71 @@ class Signup extends React.Component {
     this.setState({ [name]: value });
   };
 
+  handleSubmit = (event, signupUser) => {
+    event.preventDefault();
+    signupUser()
+      .then(data => {
+        this.clearState();
+      });
+  };
+
+  validateForm = () => {
+    const { username, email, password, passwordConfirmation} = this.state;
+    const isInvalid = !username || !email || !password || password !== passwordConfirmation;
+    return isInvalid;
+  };
+
   render() {
     const { username, email, password, passwordConfirmation} = this.state;
     return (
       <div className="App">
         <h2 className="App">Signup</h2>
-        <form className="form">
-          <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={username}
-          onChange={this.handleChange}
-          />
-          <input
-          type="text"
-          name="email"
-          placeholder="Email address"
-          value={email}
-          onChange={this.handleChange}
-          />
-          <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={this.handleChange}
-          />
-          <input
-          type="password"
-          name="passwordConfirmation"
-          placeholder="Confirm password"
-          value={passwordConfirmation}
-          onChange={this.handleChange}
-          />
-          <button type="submit" className="button-primary">Submit</button>
-        </form>
+        <Mutation
+          mutation={SIGNUP_USER}
+          variables={{ username, email, password }} >
+          {(signupUser, { data, loading, error }) => {
+            return (
+              <form
+                className="form"
+                onSubmit={event => this.handleSubmit(event, signupUser)} >
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  name="passwordConfirmation"
+                  placeholder="Confirm password"
+                  value={passwordConfirmation}
+                  onChange={this.handleChange}
+                />
+                <button
+                  type="submit"
+                  disabled={loading || this.validateForm()}
+                  className="button-primary"
+                >Submit</button>
+                {error &&  <Error error={error} />}
+              </form>
+            )
+          }}
+        </Mutation>
       </div>
     )
   }
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -1,5 +1,6 @@
 import { gql } from 'apollo-boost';
 
+/* Recipes Queries */
 export const GET_ALL_RECIPES = gql`
 query {
   getAllRecipes {
@@ -9,6 +10,35 @@ query {
     category
     likes
     createdDate
+  }
+}
+`;
+
+/* Recipes Mutations */
+
+
+/* User Queries */
+export const GET_ALL_USERS = gql`
+query {
+  getAllUsers {
+    username
+    email
+    joinDate
+    favorites {
+      _id
+      createdDate
+      likes
+      username
+    }
+  }
+}
+`;
+
+/* User Mutations */
+export const SIGNUP_USER = gql`
+mutation($username: String!, $email: String!, $password: String!) {
+  signupUser(username: $username, email: $email, password: $password){
+    token
   }
 }
 `;
