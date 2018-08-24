296c8565dc00b14e69fc498785a253aeb0519171
iskilled <iskilled@gmail.com>
2018-08-24 17:31:33 +0100

    Fix layout in Recipe Page

--- client/src/components/Recipe/RecipePage.js
+++ client/src/components/Recipe/RecipePage.js
@@ -7,24 +7,26 @@ import { GET_RECIPE } from '../../queries';
 const RecipePage = ({ match }) => {
   const { _id } = match.params;
   return (
-  <Query query={GET_RECIPE} variables={{ _id }}>
-    {({ data, loading, error }) => {
-      if (loading) return <div><span role="img" aria-label="Hourglass">⏳</span> Loading...</div>
-      if (error) return <div>Error</div>
-      return (
-        <Fragment>
-          <div className="App">Recipe Page</div>
-          <h2>{data.getRecipe.name}</h2>
-          <p>Category: {data.getRecipe.category}</p>
-          <p>Description: {data.getRecipe.description}</p>
-          <p>Instructions: {data.getRecipe.instructions}</p>
-          <p>Likes: {data.getRecipe.likes}</p>
-          <p>Created By: {data.getRecipe.username}</p>
-          <button>Like</button>
-        </Fragment>
-      );
-    }}
-  </Query>
+  <div className="App">
+    <h1>Recipe Page</h1>
+    <Query query={GET_RECIPE} variables={{ _id }}>
+      {({ data, loading, error }) => {
+        if (loading) return <div><span role="img" aria-label="Hourglass">⏳</span> Loading...</div>
+        if (error) return <div>Error</div>
+        return (
+            <Fragment>
+              <h2>{data.getRecipe.name}</h2>
+              <p>Category: {data.getRecipe.category}</p>
+              <p>Description: {data.getRecipe.description}</p>
+              <p>Instructions: {data.getRecipe.instructions}</p>
+              <p>Likes: {data.getRecipe.likes}</p>
+              <p>Created By: {data.getRecipe.username}</p>
+              <button>Like</button>
+            </Fragment>
+        );
+      }}
+    </Query>
+  </div>
   );
 };
 
