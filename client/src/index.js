9050bb439245ed249ec0af819a936568a9cb6446
iskilled <iskilled@gmail.com>
2018-08-24 01:38:02 +0100

    Add Navbar component with Links for auth or unauthed users. Add sign out button functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -12,9 +12,13 @@ import {
 import './index.css';
 
 import App from './components/App';
+import Navbar from './components/Navbar';
+import withSession from './components/withSession';
+import Search from './components/Recipe/Search';
 import Signin from './components/Auth/Signin';
 import Signup from './components/Auth/Signup';
-import withSession from './components/withSession';
+import AddRecipe from './components/Recipe/AddRecipe';
+import Profile from './components/Profile/Profile';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4444/graphql',
@@ -40,20 +44,26 @@ const client = new ApolloClient({
   }
 });
 
-const Root = ({ refetch }) => (
+const Root = ({ refetch, session }) => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route
-        path="/signin"
-        render={() => <Signin refetch={refetch} />}
-      />
-      <Route
-        path="/signup"
-        render={() => <Signup refetch={refetch} />}
-      />
-      <Redirect to="/" />
-    </Switch>
+    <Fragment>
+      <Navbar session={session} />
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/search" exact component={Search} />
+        <Route path="/recipe/add" exact component={AddRecipe} />
+        <Route path="/profile" exact component={Profile} />
+        <Route
+          path="/signin"
+          render={() => <Signin refetch={refetch} />}
+        />
+        <Route
+          path="/signup"
+          render={() => <Signup refetch={refetch} />}
+        />
+        <Redirect to="/" />
+      </Switch>
+    </Fragment>
   </Router>
 );
 
