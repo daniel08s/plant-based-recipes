5ae39071b6033b4c0b315e5aaf32e019d3b93f20
iskilled <iskilled@gmail.com>
2018-08-28 22:18:47 +0100

    Prepare deployment to heroku

--- client/src/index.js
+++ client/src/index.js
@@ -22,7 +22,7 @@ import RecipePage from './components/Recipe/RecipePage';
 import Profile from './components/Profile/Profile';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4444/graphql',
+  uri: 'https://plant-based-recipes.herokuapp.com/graphql',
   fetchOptions: {
     credentials: 'include'
   },
