c47cccffa807bf0a038e8fb1979e7b5f1730bab8
iskilled <iskilled@gmail.com>
2018-09-05 19:51:56 +0100

    Add react-pose animations to home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 // npm packages
 import React from 'react';
 import { Query } from 'react-apollo';
+import posed from 'react-pose';
 
 // our packages
 import { GET_ALL_RECIPES } from '../queries';
@@ -9,25 +10,57 @@ import RecipeItem from './Recipe/RecipeItem';
 // styling
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <h1 className="main-title">
-      Find recipes you <strong>love <span role="img" aria-label="Heart">❤</span></strong>
-    </h1>
-    <Query query={GET_ALL_RECIPES}>
-      {({ data, loading, error }) => {
-        if (loading) return <div><span role="img" aria-label="Hourglass">⏳</span> Loading...</div>
-        if (error) return <div><span role="img" aria-label="StopSign">🛑</span> Error</div>
-        return (
-          <ul className="cards">
-            {data.getAllRecipes.map(recipe => (
-              <RecipeItem key={recipe._id} {...recipe} />
-            ))}
-          </ul>
-        )
-      }}
-    </Query>
-  </div>
-);
+const RecipeList = posed.ul({
+  shown: {
+    x: '0%',
+    staggerChildren: 100
+  },
+  hidden: {
+    x: '-100%'
+  }
+});
+
+class App extends React.Component {
+  state = {
+    on: false
+  };
+
+  componentDidMount() {
+    setTimeout(this.slideIn, 200);
+  }
+
+  slideIn = () => {
+    this.setState({ on: !this.state.on });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="main-title">
+          Find recipes you <strong>love <span role="img" aria-label="Heart">❤</span></strong>
+        </h1>
+        <Query query={GET_ALL_RECIPES}>
+          {({ data, loading, error }) => {
+            if (loading) return <div><span role="img" aria-label="Hourglass">⏳</span> Loading...</div>
+            if (error) return <div><span role="img" aria-label="StopSign">🛑</span> Error</div>
+            
+            const { on } = this.state;
+
+            return (
+              <RecipeList
+                className="cards"
+                pose={ on ? 'shown' : 'hidden' }
+              >
+                {data.getAllRecipes.map(recipe => (
+                  <RecipeItem key={recipe._id} {...recipe} />
+                ))}
+              </RecipeList>
+            )
+          }}
+        </Query>
+      </div>
+    );
+  }
+};
 
 export default App;
