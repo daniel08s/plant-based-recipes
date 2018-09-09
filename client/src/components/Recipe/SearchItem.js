8cee12e3ea85fe92816748585227df957a401814
iskilled <iskilled@gmail.com>
2018-09-09 19:17:27 +0100

    Refactor SearchItem component

--- client/src/components/Recipe/SearchItem.js
+++ client/src/components/Recipe/SearchItem.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import posed from 'react-pose';
 
-const SearchItem = posed.li({
-
-});
-
-export default ({ _id, name, likes, imageUrl }) => (
-    <SearchItem 
-      style={{ background: `url(${imageUrl}) center center / cover no-repeat` }}
-      className="card"
-    >
-      <div className="card-text">
-        <Link to={`/recipes/${_id}`}>
-          <h4>{name}</h4>
-        </Link>
-        <span>{likes} <span role="img" aria-label="Heart">❤</span></span>
-      </div>
-    </SearchItem>
+const SearchItem = ({ _id, name, likes, imageUrl }) => (
+  <li 
+    style={{ background: `url(${imageUrl}) center center / cover no-repeat` }}
+    className="card"
+  >
+    <div className="card-text">
+      <Link to={`/recipes/${_id}`}>
+        <h4>{name}</h4>
+      </Link>
+      <span>{likes} <span role="img" aria-label="Heart">❤</span></span>
+    </div>
+  </li>
 );
+
+export default SearchItem;
